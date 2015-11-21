caa10616277010da8889c40bc7ebc5199a494a93
Sumayyah Perine <sperine01@gmail.com>
2015-11-21 12:18:10 -0500

    added context to rspec tests

--- rspechomework.rb
+++ rspechomework.rb
@@ -14,38 +14,59 @@ describe Article do
     let(:empty_article) { Article.new(title: "", body: "", author: "") }
 
 
-    it "returns true of title string is present" do
+    context "#title?" do
+        it "returns true of title string is present" do
             expect(article.title?).to eq(true)
+        end
     end
 
 
-    it "returns true if string is present" do
-        expect(article.body?).to eq(true)
+    context "#body?" do
+        it "returns true if string is present" do
+            expect(article.body?).to eq(true)
+        end
     end
 
 
-    it "returns false if authors_name_starts_with_q?" do
-        expect(article.authors_name_starts_with_q).to eq(false)
+    context "#authors_name_starts_with_q?" do
+            it "returns false if authors_name_starts_with_q?" do
+                expect(article.authors_name_starts_with_q).to eq(false)
+        end
     end
 
 
-    it "returns title, author, body as html"
-        expect(article.article_as_html).to eq("<h1>Squishy</h1><h3>May-May</h3><p>blah blah blah blah</p>")
+    context "#string_empty?" do
+            it "returns false if there is a string present" do
+                expect(article.string_empty?("")).to eq(false)
+        end
     end
 
 
-    it "returns title as html" do
-        expect(article.title_as_html).to eq("<h1>Squishy</h1>")
+    context "#article_as_html" do
+        it "returns title, author, body as html"
+            expect(article.article_as_html).to eq("<h1>Squishy</h1><h3>May-May</h3><p>blah blah blah blah</p>")
+        end
     end
 
 
-    it "returns author as html" do
-        expect(article.author_as_html).to eq("<b>May-May</b>")
+    context "#title_as_html" do
+        it "returns title as html" do
+            expect(article.title_as_html).to eq("<h1>Squishy</h1>")
+        end
     end
 
 
-    it "returns body as html" do
-        expect(article.body_as_html).to eq("<p>blah blah blah blah</p>")
+    context "#author_as_html" do
+        it "returns author as html" do
+            expect(article.author_as_html).to eq("<b>May-May</b>")
+        end
+    end
+
+
+    context "#body_as_html" do
+        it "returns body as html" do
+            expect(article.body_as_html).to eq("<p>blah blah blah blah</p>")
+        end
     end
 
 
@@ -65,24 +86,29 @@ describe Transaction do
         end
     end
 
-    # context "valid_amount?" do
-        it " return valid_amount?" do
+    context "valid_amount?" do
+        it "returns the valid amount" do
             expect(transaction.valid_amount?(transaction.amount)).to eq(true)
         end
+    end
 
-    # context "valid_time?" do
-        it "return true if valid_time?" do
+    context "valid_time?" do
+        it "return true if the time is valid" do
             expect(transaction.valid_time(transaction.time(Time.now))).to eq(true)
         end
+    end
 
-    # context "amount_to_currency" do
-        it "amount_to_currency" do
+    context "amount_to_currency" do
+        it "changes the amount in currency" do
             expect(transaction.amount_to_currency).to eq(1000)
         end
+    end
 
 
-        it "pretty_time" do
+    context "pretty_time" do
+        it "displays the time format" do
             expect(transaction.pretty_time).to eq(Time.now).strftime("%A, %d %b %Y %l:%M %p")
         end
+    end
 
 end
